cb22e7c3f4754872d6cf24e0f00e805c7945f119
Friendlyflame1 <yash.goel@bntechnology.net>
2024-01-19 17:33:06 +0530

    piechart data

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 5000;
 // chart data values
 const xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
 const yArray = [55, 49, 44, 24, 15];
-const data = [{labels:xArray, values:yArray, type:"pie"}];
+const data = {labels:xArray, values:yArray,};
 
 app.use(cors());
 // Define a route to fetch data
